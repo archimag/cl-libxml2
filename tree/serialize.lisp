83e948023a074b13faed0809795cdaf1af7ebdb5
archimag <archimag@gmail.com>
2009-05-20 14:34:18 +0000

    add encoding key to tree:serialize

--- tree/serialize.lisp
+++ tree/serialize.lisp
@@ -6,7 +6,7 @@
 ;;; serialize
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defgeneric serialize (obj target))
+(defgeneric serialize (obj target &key))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; serialize ((doc document) (filename pathname))
@@ -16,9 +16,17 @@
   (filename :pointer)
   (doc %xmlDocPtr))
 
-(defmethod serialize ((doc document) (filename pathname))
-  (with-foreign-string (%path (format nil "~A" filename))
-    (%xmlSaveFile %path (pointer doc))))
+(define-libxml2-function ("xmlSaveFileEnc" %xmlSaveFileEnc) :int
+  (filename :pointer)
+  (doc %xmlDocPtr)
+  (encoding %xmlCharPtr))
+
+(defmethod serialize ((doc document) (filename pathname) &key (encoding :utf-8))
+  (with-foreign-strings ((%path (format nil "~A" filename))
+                         (%encoding (format nil "~A" encoding)))
+    (%xmlSaveFileEnc %path
+                     (pointer doc)
+                     %encoding)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; serialize ((doc document) (s (eql :to-string)))
@@ -31,14 +39,14 @@
   (txt_encoding :pointer)
   (format :int))
 
-(defmethod serialize ((doc document) (s (eql :to-string)))
-  (with-foreign-string (%utf-8 "utf-8")
+(defmethod serialize ((doc document) (s (eql :to-string)) &key (encoding :utf-8))
+  (with-foreign-string (%encoding (format nil "~A" encoding))
     (with-foreign-pointer (%xml-string (foreign-type-size :pointer))
       (with-foreign-pointer (%xml-string-len (foreign-type-size :pointer))
         (%xmlDocDumpFormatMemoryEnc (pointer doc)
                                     %xml-string
                                     %xml-string-len
-                                    %utf-8 ;;(null-pointer)
+                                    %encoding
                                     1)
         (let ((%ptr (mem-ref %xml-string :pointer)))
           (unwind-protect 
@@ -81,28 +89,33 @@
 
 (define-libxml2-function ("xmlOutputBufferClose" %xmlOutputBufferClose) :int
   (buf %xmlOutputBufferPtr))
-  
 
-(defmethod serialize ((doc document) (stream stream))
-  (with-foreign-string (%utf-8 "utf-8")
+(defctype %xmlCharEncodingHandlerPtr :pointer)
+
+(define-libxml2-function ("xmlFindCharEncodingHandler" %xmlFindCharEncodingHandler) %xmlCharEncodingHandlerPtr
+  (name %xmlCharPtr))
+
+(defmethod serialize ((doc document) (stream stream) &key (encoding :utf-8))
+  (with-foreign-string (%encoding (format nil "~A" encoding))
     (let ((*stream-for-xml-serialize* stream))
       (%xmlSaveFileTo (%xmlOutputBufferCreateIO (%stream-writer-callback stream)
                                                 (null-pointer)
                                                 (null-pointer)
-                                                (null-pointer))
+                                                (%xmlFindCharEncodingHandler %encoding))
                       (pointer doc)
-                      %utf-8))))
+                      %encoding))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; serialize ((el node) target)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod serialize ((el node) target)
+(defmethod serialize ((el node) target &key (encoding :utf-8))
   (case  (node-type el)
     (:xml-element-node (with-fake-document (doc el)
                          (serialize doc target)))
     (:xml-document-node (serialize (make-instance 'document
                                                   :pointer (pointer el))
-                                   target))
+                                   target
+                                   :encoding encoding))
     (otherwise (error "Bad node type: ~A" (node-type el)))))
      
